5b534c5c863f6b1e264edfe9c613bc2a2e55c6e1
Elisiário Couto <elisiario@couto.io>
2024-02-14 23:22:33 +0000

    tests: Add basic test script.

---
+++ tests/model.md
@@ -0,0 +1,95 @@
+# Car
+
+This is the description of the Car.
+
+New lines work.
+UTF-8 characters work: áéíóú
+👍
+
+| Property | Type | Required | Possible Values | Deprecated | Default | Description |
+| -------- | ---- | -------- | --------------- | ---------- | ------- | ----------- |
+| brand | `string` | ✅ | string|  |  | The brand of the car. |
+| model | `string` | ✅ | string|  |  | The model of the car. |
+| year | `integer` | ✅ | integer|  |  | The year of the car. |
+| car_class | `object` | ✅ | [CarClass](#carclass)|  |  | The class of the car. |
+| engine | `object` | ✅ | [Engine](#engine)|  |  | The engine of the car. |
+| color | `string` | ✅ | string|  |  | The color of the car. |
+| kms | `integer` |  | integer|  |  | The number of kilometers the car has. |
+| manufacterer_config | `object` |  | [Airbag](#airbag) or [NavigationSystem](#navigationsystem) or [Upholstery](#upholstery)|  |  | The manufacturer's extras. |
+| brand_country | `string` | ✅ | [`^[A-Z]{2}$`](https://regex101.com/?regex=%5E%5BA-Z%5D%7B2%7D%24)| ⛔️ |  | [Deprecated] The country where the brand is from. |
+
+
+---
+
+# Definitions
+
+
+
+## Airbag
+
+This is the description of the AirbagExtra.
+
+**Type:** `object`
+
+| Property | Type | Required | Possible Values | Deprecated | Default | Description |
+| -------- | ---- | -------- | --------------- | ---------- | ------- | ----------- |
+| type | `string` | ✅ | `front` `side` `curtain`|  |  | The type of airbag. |
+
+
+## Class
+
+This is the description of the CarClass.
+
+**Type:** `object`
+
+| Property | Type | Required | Possible Values | Deprecated | Default | Description |
+| -------- | ---- | -------- | --------------- | ---------- | ------- | ----------- |
+| type | `string` | ✅ | `sedan` `hatchback` `suv`|  |  | The type of car. |
+| doors | `integer` |  | integer|  | `5` | The number of doors the car has. |
+| passengers | `integer` |  | integer|  | `5` | The number of passengers the car can carry. |
+
+
+## Engine
+
+This is the description of the Engine.
+
+**Markdown works**. *Italic*. **Bold**. ***Bold and italic***.
+- [] Unchecked
+- [x] Checked
+
+**Type:** `object`
+
+| Property | Type | Required | Possible Values | Deprecated | Default | Description |
+| -------- | ---- | -------- | --------------- | ---------- | ------- | ----------- |
+| model | `string` | ✅ | string|  |  | The name of the engine model. |
+| power | `integer` | ✅ | integer|  |  | The power of the engine in HP. |
+| fuel_type | `string` | ✅ | `gasoline` `diesel` `electric`|  |  | The type of fuel the engine uses. |
+| liters | `number` | ✅ | number|  |  | The displacement of the engine in liters. |
+| turbo | `boolean` |  | boolean|  |  | Whether the engine has a turbo or not. |
+
+
+## NavigationSystem
+
+This is the description of the NavigationSystem.
+
+**Type:** `object`
+
+| Property | Type | Required | Possible Values | Deprecated | Default | Description |
+| -------- | ---- | -------- | --------------- | ---------- | ------- | ----------- |
+| type | `string` | ✅ | `gps` `carplay` `androidauto`|  |  | The type of navigation system. |
+
+
+## Upholstery
+
+This is the description of the Upholstery.
+
+**Type:** `object`
+
+| Property | Type | Required | Possible Values | Deprecated | Default | Description |
+| -------- | ---- | -------- | --------------- | ---------- | ------- | ----------- |
+| type | `string` | ✅ | `leather` `fabric`|  |  | The type of upholstery. |
+
+
+---
+
+Markdown generated with [jsonschema-markdown](https://github.com/elisiariocouto/jsonschema-markdown).
---
+++ tests/model.py
@@ -0,0 +1,169 @@
+import json
+from typing import Literal
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class CarClass(BaseModel):
+    """
+    This is the description of the CarClass.
+    """
+
+    type: Literal["sedan", "hatchback", "suv"] = Field(
+        ..., description="The type of car."
+    )
+    doors: int = Field(default=5, description="The number of doors the car has.")
+    passengers: int = Field(
+        default=5, description="The number of passengers the car can carry."
+    )
+
+    model_config = ConfigDict(title="Class")
+
+
+class Engine(BaseModel):
+    """
+    This is the description of the Engine.
+
+    **Markdown works**. *Italic*. **Bold**. ***Bold and italic***.
+    - [] Unchecked
+    - [x] Checked
+    """
+
+    model: str = Field(
+        ...,
+        description="The name of the engine model.",
+        max_length=100,
+        min_length=1,
+    )
+    power: int = Field(..., description="The power of the engine in HP.")
+    fuel_type: Literal["gasoline", "diesel", "electric"] = Field(
+        ..., description="The type of fuel the engine uses."
+    )
+    turbo: bool = Field(
+        default=False,
+        description="Whether the engine has a turbo or not.",
+    )
+    liters: float = Field(
+        description="The displacement of the engine in liters.",
+        gt=0,
+    )
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "examples": [
+                {
+                    "model": "1.6 TDI",
+                    "power": 105,
+                    "fuel_type": "diesel",
+                    "turbo": True,
+                    "liters": 1.6,
+                },
+                {
+                    "model": "1.4 TSI",
+                    "power": 150,
+                    "fuel_type": "gasoline",
+                    "turbo": True,
+                    "liters": 1.4,
+                },
+                {
+                    "model": "e-208",
+                    "power": 136,
+                    "fuel_type": "electric",
+                    "turbo": False,
+                    "liters": 0,
+                },
+            ]
+        }
+    )
+
+
+class Airbag(BaseModel):
+    """
+    This is the description of the AirbagExtra.
+    """
+
+    type: Literal["front", "side", "curtain"] = Field(
+        ..., description="The type of airbag."
+    )
+
+
+class NavigationSystem(BaseModel):
+    """
+    This is the description of the NavigationSystem.
+    """
+
+    type: Literal["gps", "carplay", "androidauto"] = Field(
+        ..., description="The type of navigation system."
+    )
+
+
+class Upholstery(BaseModel):
+    """
+    This is the description of the Upholstery.
+    """
+
+    type: Literal["leather", "fabric"] = Field(
+        ..., description="The type of upholstery."
+    )
+
+
+class Car(BaseModel):
+    """
+    This is the description of the Car.
+
+    New lines work.
+    UTF-8 characters work: áéíóú
+    👍
+    """
+
+    brand: str = Field(
+        ...,
+        description="The brand of the car.",
+        max_length=100,
+        min_length=1,
+        examples=["Ford", "Toyota"],
+    )
+    brand_country: str = Field(
+        description="[Deprecated] The country where the brand is from.",
+        pattern="^[A-Z]{2}$",
+    )
+    model: str = Field(
+        ...,
+        description="The model of the car.",
+        max_length=100,
+        min_length=1,
+        examples=[
+            """
+            ### Mardown works here as well
+            - yes
+            - no
+            """
+        ],
+    )
+    year: int = Field(
+        ...,
+        description="The year of the car.",
+        gt=1900,
+        lt=2100,
+    )
+    car_class: CarClass = Field(
+        ...,
+        description="The class of the car.",
+    )
+    engine: Engine = Field(
+        ...,
+        description="The engine of the car.",
+    )
+    kms: int = Field(None, description="The number of kilometers the car has.")
+    color: str = Field(
+        ...,
+        description="The color of the car.",
+        max_length=100,
+        min_length=1,
+    )
+    manufacterer_config: list[Airbag | NavigationSystem | Upholstery] = Field(
+        default=[], description="The manufacturer's extras."
+    )
+
+
+print(json.dumps(Car.model_json_schema()))
